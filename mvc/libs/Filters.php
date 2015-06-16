9d58dbe01d21d0aaf80afb7bfe5e74c9af6498c1
Chemaclass <chemaclass@outlook.es>
2015-06-17 00:06:17 +0200

    Add libs/Filters file

---
+++ mvc/libs/Filters.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Libs;
+
+/**
+ * Filters from Wordpress
+ *
+ * @author José María Valera Reales
+ */
+class Filters {
+	
+	/**
+	 * Just one function as example
+	 */
+	public static function wpFilterTest() {
+		// some code...
+	}
+}
