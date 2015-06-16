d473493cc74c7dbaaa1a0da8ab8df8159a57eb3e
Chemaclass <chemaclass@outlook.es>
2015-06-16 23:29:41 +0200

    Move all actions from functions file to Action file

--- functions.php
+++ functions.php
@@ -4,39 +4,26 @@ require_once 'vendor/autoload.php';
 use Libs\Actions;
 use Libs\Filters;
 
+// --------------------------------------------------------------
+// Some constants
+// --------------------------------------------------------------
 define(URL_PRO, 'knob.chemaclass.com');
 define(URL_DEV, 'knob.chemaclass.com');
 define(URL_LOC, 'knob.chemaclass.local');
+define(PUBLIC_DIR, get_template_directory_uri() . '/public');
+define(COMPONENTS_DIR, get_template_directory_uri() . '/vendor/components');
 
 // --------------------------------------------------------------
 // Actions
 // --------------------------------------------------------------
+Actions::adminPrintScripts();
+Actions::adminPrintStyles();
 Actions::wpBeforeAdminBarRender();
+
 // --------------------------------------------------------------
 // Filters
 // --------------------------------------------------------------
-// and so on...
-
-$publicDir = get_template_directory_uri() . '/public';
-$componentsDir = get_template_directory_uri() . '/vendor/components';
-
-/**
- * Put styles into the admin view
- */
-add_action('admin_print_styles', function () use($publicDir) {
-	wp_enqueue_style('bootstrap', $componentsDir . '/bootstrap/css/bootstrap.css');
-	wp_enqueue_style('font-awesome', $componentsDir . '/font-awesome/css/font-awesome.min.css');
-	wp_enqueue_style('main', $publicDir . '/css/main.css');
-});
-
-/**
- * Put scripts into the admin view
- */
-add_action('admin_print_scripts', function () use($publicDir) {
-	wp_enqueue_script('jquery-plugin', $componentsDir . '/jquery/jquery.min.js');
-	wp_enqueue_script('bootstrap-plugin', $componentsDir . '/bootstrap/js/bootstrap.min.js');
-	wp_enqueue_script('nm', $publicDir . '/js/main.js');
-});
+Filters::wpFilterTest();
 
 /**
  * Remove the admin bar in prod
