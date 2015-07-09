00d3b5a643ded3c205b94fb411a31cf9c3783234
Chemaclass <chemaclass@outlook.es>
2015-07-09 13:13:26 +0200

    Add page-lang file

---
+++ page-lang.php
@@ -0,0 +1,31 @@
+<?php
+use I18n\I18n;
+
+/**
+ * NOTE:
+ * For to use this page remember that you have to create your own
+ * page through the backend from Wordpress.
+ *
+ * Example to use:
+ * yoursite.com/lang?lang=en
+ */
+
+$lang = $_GET['lang'];
+$redirect = $_GET['redirect'];
+
+/*
+ * Check if the lang is available
+ */
+if (in_array($lang, I18n::getAllLangAvailable())) {
+	session_start();
+	$_SESSION[I18n::CURRENT_LANG] = $lang;
+}
+
+/*
+ * We only redirect the url if not is absolute
+ */
+if (!$redirect || strpos($redirect, 'http') !== false || strpos($redirect, 'https') !== false) {
+	header("Location: /");
+} else {
+	header("Location: $redirect");
+}
