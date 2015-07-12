9db257cd8042bf12fa69ba20bebf31333152209e
Chemaclass <chemaclass@outlook.es>
2015-07-12 15:27:18 +0200

    Add dd, debug and info func into Helper file

--- mvc/libs/Helpers.php
+++ mvc/libs/Helpers.php
@@ -1,2 +1,30 @@
 <?php
 
+/**
+ *
+ * @param string|array $expression
+ * @param string $tag
+ */
+function dd($expression, $tag = "Tag") {
+	echo '' . $tag . '<br>';
+	var_dump($expression);
+	exit();
+}
+
+/**
+ * Cadena para debug
+ *
+ * @param string $str
+ */
+function debug($str) {
+	error_log(" DEBUG - " . $str);
+}
+
+/**
+ * Cadena para info 'debug'
+ *
+ * @param string $str
+ */
+function info($str) {
+	error_log(" INFO - " . $str);
+}
