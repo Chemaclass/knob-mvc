fca741991888c9b5dc0c138a413b8223203b6b7c
Chemaclass <chemaclass@outlook.es>
2015-07-15 19:51:36 +0200

    bugfixed: i18n en/internal>blog_description

--- mvc/i18n/en/internal.php
+++ mvc/i18n/en/internal.php
@@ -1,5 +1,5 @@
 <?php
 return [
 	'blog_description' => 'Knob is a PHP MVC Framework for creating Wordpress
-		templates easier and with more fun than ever before.'
+		templates easier and funnier than ever before.'
 ];
