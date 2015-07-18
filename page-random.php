30428b8a2143f906c4f68508db3e275d59814fc4
Chemaclass <chemaclass@outlook.es>
2015-07-18 11:18:36 +0200

    Add page-random.php

---
+++ page-random.php
@@ -0,0 +1,21 @@
+<?php
+use Models\Post;
+
+/**
+ * NOTE:
+ * For to use this page remember that you have to create your own
+ * page through the backend from Wordpress.
+ *
+ * @example yoursite.com/lang?lang=en
+ * @see https://codex.wordpress.org/Template_Tags/get_posts
+ */
+$posts = get_posts([
+	'post_status' => Post::STATUS_PUBLISH,
+	'post_type' => Post::TYPE_POST,
+	'showposts' => 1,
+	'orderby' => 'rand'
+]);
+
+$post = Post::find($posts[0]->ID);
+
+header("Location: {$post->getPermalink()}");
