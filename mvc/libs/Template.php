771c7136d4ba455dad08072475169c07cae2e913
Chemaclass <chemaclass@outlook.es>
2015-07-30 13:59:22 +0200

    Add Template libs

---
+++ mvc/libs/Template.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Libs;
+
+use Mustache_Engine;
+use Mustache_Loader_FilesystemLoader;
+use Mustache_Logger_StreamLogger;
+use I18n\I18n;
+
+/**
+ * Template singleton
+ *
+ * @author José María Valera Reales
+ */
+class Template {
+
+	/*
+	 * Const
+	 */
+	const CACHE_FILE_MODE = 0660;
+	const CACHE_LAMBDA_TEMPLATES = true;
+	const CHARSET = 'UTF-8';
+	const STRICT_CALLABLES = true;
+
+	/*
+	 * Singleton
+	 */
+	private static $instance = null;
+
+	/**
+	 * Constructor
+	 */
+	private function __construct() {
+	}
+
+	/**
+	 * Return the relative path location where are the templates.
+	 *
+	 * @return string
+	 */
+	private static function getTemplatesFolderLocation() {
+		return str_replace('//', '/', dirname(__FILE__) . '/') . '../templates';
+	}
+
+	/**
+	 *
+	 * @return NULL
+	 */
+	public static function getInstance() {
+		if (!static::$instance) {
+			$templatesFolder = static::getTemplatesFolderLocation();
+			static::$instance = new Mustache_Engine([
+				'charset' => static::CHARSET,
+				'strict_callables' => static::STRICT_CALLABLES,
+				'cache_file_mode' => static::CACHE_FILE_MODE,
+				'cache_lambda_templates' => static::CACHE_LAMBDA_TEMPLATES,
+				'loader' => new Mustache_Loader_FilesystemLoader($templatesFolder),
+				'partials_loader' => new Mustache_Loader_FilesystemLoader($templatesFolder),
+				'logger' => new Mustache_Logger_StreamLogger('php://stderr'),
+				'helpers' => self::getHelpers(),
+				'pragmas' => static::getPragmas(),
+				'escape' => function ($value) {
+					return htmlspecialchars($value, ENT_COMPAT, static::CHARSET);
+				}
+			]);
+		}
+		return static::$instance;
+	}
+
+	/**
+	 *
+	 * @return multitype:string
+	 */
+	private static function getPragmas() {
+		return [
+			Mustache_Engine::PRAGMA_FILTERS,
+			Mustache_Engine::PRAGMA_BLOCKS
+		];
+	}
+
+	/**
+	 *
+	 * @return multitype:NULL multitype:NULL |string|boolean
+	 */
+	private static function getHelpers() {
+		return [
+			'trans' => function ($value) {
+				return I18n::trans($value);
+			},
+			'transu' => function ($value) {
+				return I18n::transu($value);
+			},
+			'case' => [
+				'lower' => function ($value) {
+					return strtolower((string) $value);
+				},
+				'upper' => function ($value) {
+					return strtoupper((string) $value);
+				}
+			],
+			'count' => function ($value) {
+				return count($value);
+			},
+			'moreThan1' => function ($value) {
+				return count($value) > 1;
+			},
+			'date' => [
+				'xmlschema' => function ($value) {
+					return date('c', strtotime($value));
+				},
+				'string' => function ($value) {
+					return date('l, d F Y', strtotime($value));
+				},
+				'format' => function ($value) {
+					return date(get_option('date_format'), strtotime($value));
+				}
+			],
+			'toArray' => function ($value) {
+				return explode(',', $value);
+			},
+			'ucfirst' => function ($value) {
+				return ucfirst($value);
+			}
+		];
+	}
+}
