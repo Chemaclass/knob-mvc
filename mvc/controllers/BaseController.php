e13ca05f7cbee54903eb089db542826517c91a50
Chemaclass <chemaclass@outlook.es>
2015-08-01 18:31:53 +0200

    remove all templateVars from BaseController. Using from configParams

--- mvc/controllers/BaseController.php
+++ mvc/controllers/BaseController.php
@@ -3,7 +3,6 @@
 namespace Controllers;
 
 use Libs\Utils;
-use Libs\Env;
 use Models\User;
 use Models\Post;
 use I18n\I18n;
@@ -35,7 +34,7 @@ abstract class BaseController {
 	 * Constructor
 	 */
 	public function __construct() {
-		$this->configParams = Params::getInstance()->all();
+		$this->configParams = Params::all();
 		$this->currentUser = User::getCurrent();
 		$this->template = Template::getInstance()->getRenderEngine();
 		$this->widgets = [ ];
@@ -81,57 +80,14 @@ abstract class BaseController {
 		$templateVars['tags'] = Term::getTags();
 
 		/*
+		 * Current User
+		 */
+		$templateVars['currentUser'] = $this->currentUser;
+
+		/*
 		 * Generics variables
 		 */
-		return array_merge($templateVars, [
-			'adminEmail' => ADMIN_EMAIL,
-			'atomUrl' => get_bloginfo('atom_url'),
-
-			'blogTitle' => BLOG_TITLE,
-			'blogDescription' => ($d = I18n::trans('internal.blog_description')) ? $d : get_bloginfo('description'),
-
-			'categoryBase' => ($c = get_option('category_base')) ? $c : Post::CATEGORY_BASE_DEFAULT,
-			'charset' => get_bloginfo('charset'),
-			'commentsAtomUrl' => get_bloginfo('comments_atom_url'),
-			'commentsRss2Url' => get_bloginfo('comments_rss2_url'),
-			'componentsDir' => COMPONENTS_DIR,
-			'currentLang' => I18n::getLangBrowserByCurrentUser(),
-			'currentLangFullname' => I18n::getLangFullnameBrowserByCurrentUser(),
-			'currentUser' => $this->currentUser,
-
-			'homeUrl' => get_home_url(),
-			'htmlType' => get_bloginfo('html_type'),
-
-			'isEnvProd' => Env::isProd(),
-			'isEnvDev' => Env::isDev(),
-			'isEnvLoc' => Env::isLoc(),
-			'isUserLoggedIn' => is_user_logged_in(),
-
-			'language' => get_bloginfo('language'),
-			'loginUrl' => wp_login_url($_SERVER['REQUEST_URI']),
-
-			'name' => get_bloginfo('name'),
-
-			'pingbackUrl' => get_bloginfo('pingback_url'),
-			'postsPerPage' => get_option('posts_per_page'),
-			'publicDir' => PUBLIC_DIR,
-
-			'rdfUrl' => get_bloginfo('rdf_url'),
-			'rss2Url' => get_bloginfo('rss2_url'),
-			'rssUrl' => get_bloginfo('rss_url'),
-
-			'stylesheetDirectory' => get_bloginfo('stylesheet_directory'),
-			'stylesheetUrl' => get_bloginfo('stylesheet_url'),
-
-			'tagBase' => ($t = get_option('tag_base')) ? $t : Post::TAG_BASE_DEFAULT,
-			'templateDirectory' => get_bloginfo('template_directory'),
-			'templateUrl' => get_bloginfo('template_url'),
-			'textDirection' => get_bloginfo('text_direction'),
-
-			'version' => get_bloginfo('version'),
-
-			'wpurl' => get_bloginfo('wpurl')
-		]);
+		return array_merge($templateVars, $this->configParams['templateVars']);
 	}
 
 	/**
