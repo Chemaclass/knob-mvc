a8088982307d39719095c2e349960527af1822c6
Chemaclass <chemaclass@outlook.es>
2015-08-02 15:46:30 +0200

    put prefix 'blog' to all Params who come from get_bloginfo(str)

--- mvc/config/Params.php
+++ mvc/config/Params.php
@@ -41,49 +41,44 @@ class Params {
 
 				'blogAuthor' => 'José María Valera Reales',
 				'blogCharset' => get_bloginfo('charset'),
-				'blogKeywords' => 'knob, wordpress, framework, mvc, template',
-				'blogName' => get_bloginfo('name'),
-				'blogTitle' => BLOG_TITLE,
+				'blogCommentsAtomUrl' => get_bloginfo('comments_atom_url'),
+				'blogCommentsRss2Url' => get_bloginfo('comments_rss2_url'),
 				'blogDescription' => ($d = I18n::trans('internal.blog_description')) ? $d : get_bloginfo('description'),
+				'blogHtmlType' => get_bloginfo('html_type'),
+				'blogLanguage' => get_bloginfo('language'),
+				'blogLoginUrl' => wp_login_url($_SERVER['REQUEST_URI']),
+				'blogKeywords' => 'knob, wordpress, framework, mvc, template, mustache, php',
+				'blogPingbackUrl' => get_bloginfo('pingback_url'),
+				'blogPostsPerPage' => get_option('posts_per_page'),
+				'blogName' => get_bloginfo('name'),
+				'blogRdfUrl' => get_bloginfo('rdf_url'),
+				'blogRss2Url' => get_bloginfo('rss2_url'),
+				'blogRssUrl' => get_bloginfo('rss_url'),
+				'blogTitle' => BLOG_TITLE,
+				'blogStylesheetDirectory' => get_bloginfo('stylesheet_directory'),
+				'blogStylesheetUrl' => get_bloginfo('stylesheet_url'),
+				'blogTagBase' => ($t = get_option('tag_base')) ? $t : Post::TAG_BASE_DEFAULT,
+				'blogTemplateDirectory' => get_bloginfo('template_directory'),
+				'blogTemplateUrl' => get_bloginfo('template_url'),
+				'blogTextDirection' => get_bloginfo('text_direction'),
+				'blogVersion' => get_bloginfo('version'),
+				'blogWpurl' => get_bloginfo('wpurl'),
 
 				'categoryBase' => ($c = get_option('category_base')) ? $c : Post::CATEGORY_BASE_DEFAULT,
-				'commentsAtomUrl' => get_bloginfo('comments_atom_url'),
-				'commentsRss2Url' => get_bloginfo('comments_rss2_url'),
 				'componentsDir' => COMPONENTS_DIR,
 				'currentLang' => I18n::getLangBrowserByCurrentUser(),
 				'currentLangFullname' => I18n::getLangFullnameBrowserByCurrentUser(),
 
 				'homeUrl' => get_home_url(),
-				'htmlType' => get_bloginfo('html_type'),
 
 				'isEnvProd' => Env::isProd(),
 				'isEnvDev' => Env::isDev(),
 				'isEnvLoc' => Env::isLoc(),
 				'isUserLoggedIn' => is_user_logged_in(),
 
-				'language' => get_bloginfo('language'),
-				'loginUrl' => wp_login_url($_SERVER['REQUEST_URI']),
-
-				'pingbackUrl' => get_bloginfo('pingback_url'),
-				'postsPerPage' => get_option('posts_per_page'),
-				'publicDir' => PUBLIC_DIR,
-
-				'rdfUrl' => get_bloginfo('rdf_url'),
-				'rss2Url' => get_bloginfo('rss2_url'),
-				'rssUrl' => get_bloginfo('rss_url'),
-
-				'stylesheetDirectory' => get_bloginfo('stylesheet_directory'),
-				'stylesheetUrl' => get_bloginfo('stylesheet_url'),
-
-				'tagBase' => ($t = get_option('tag_base')) ? $t : Post::TAG_BASE_DEFAULT,
-				'templateDirectory' => get_bloginfo('template_directory'),
-				'templateUrl' => get_bloginfo('template_url'),
-				'textDirection' => get_bloginfo('text_direction'),
-
-				'version' => get_bloginfo('version'),
-
-				'wpurl' => get_bloginfo('wpurl')
+				'publicDir' => PUBLIC_DIR
 			]
+
 		];
 	}
 }
