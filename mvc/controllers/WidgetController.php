5879fd61262d89c357611485b873a31e0a8023e2
Chemaclass <chemaclass@outlook.es>
2015-08-16 13:26:01 +0200

    Add WidgetController.php

---
+++ mvc/controllers/WidgetController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+/**
+ * Widget Controller
+ *
+ * @author José María Valera Reales
+ */
+class WidgetController extends BaseController {
+
+	/**
+	 * construct
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+}
