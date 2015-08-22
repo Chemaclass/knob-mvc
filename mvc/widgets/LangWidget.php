bbff381d0aa18ce35c988c10eeab868ec1ba0407
Chemaclass <chemaclass@outlook.es>
2015-08-22 13:53:05 +0200

    fix a english word

--- mvc/widgets/LangWidget.php
+++ mvc/widgets/LangWidget.php
@@ -12,7 +12,7 @@ class LangWidget extends WidgetBase {
 	 * All variables what we need are in Params.globalVars, and
 	 * these are autoimplement in every template.
 	 *
-	 * So this widget it's done :-)
+	 * So this widget is done :-)
 	 *
 	 */
 }
