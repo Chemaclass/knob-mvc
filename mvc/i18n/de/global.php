8eb4ea85086f817f001233f744dd4c7f03395bba
Chemaclass <chemaclass@outlook.es>
2015-08-25 23:44:45 +0200

    Add dir i18n/de but I need to translate from english to german yet

---
+++ mvc/i18n/de/global.php
@@ -0,0 +1,94 @@
+<?php
+return [
+	// A
+	'archive' => 'archive',
+	'archives' => 'archives',
+
+	// B
+	'by' => 'by',
+
+	// C
+	'category' => 'category',
+	'categories' => 'categories',
+	'comment' => 'comment',
+	'comments' => 'comments',
+	'current' => 'current',
+	'current_lang' => 'current language',
+
+	// D
+	'delete' => 'delete',
+
+	// E
+	'english' => 'english',
+	'email' => 'email',
+	'edit' => 'edit',
+	'error' => 'error',
+
+	// F
+
+	// G
+
+	// H
+	'high' => 'high',
+
+	// I
+	'in' => 'in',
+
+	// J
+
+	// K
+
+	// L
+	'lang_en' => 'english',
+	'lang_es' => 'spanish',
+	'language' => 'language',
+
+	// M
+
+	// N
+	'name' => 'name',
+	'not_found' => 'not found',
+
+	// O
+
+	// P
+	'page' => 'page',
+	'pages' => 'pages',
+	'post' => 'post',
+	'posts' => 'posts',
+
+	// Q
+
+	// R
+	'random' => 'random',
+
+	// S
+	'search' => 'search',
+	'searcher' => 'searcher',
+	'see_more' => 'see more',
+	'share_comment' => 'share comment',
+	'show_more' => 'show more',
+	'spanish' => 'spanish',
+
+	// T
+	'tag' => 'tag',
+	'tags' => 'tags',
+	'total' => 'total',
+
+	// U
+
+	// V
+
+	// W
+	'website' => 'website',
+	'welcome' => 'welcome',
+	'width' => 'width',
+	'without_permissions' => 'without permissions',
+
+	// X
+
+	// Y
+
+	// Z
+	'z' => 'z'
+];
---
+++ mvc/i18n/de/internal.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	'blog_description' => 'Knob is a PHP MVC Framework for creating Wordpress
+		templates easier and funnier than ever before.'
+];
