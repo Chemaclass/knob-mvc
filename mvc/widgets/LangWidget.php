da3394347c6c0898516f3994a4525bd35fe66b59
Chemaclass <chemaclass@outlook.es>
2015-09-05 17:18:35 +0200

    Pass the langs available (from I18n) to LangWidget

--- mvc/widgets/LangWidget.php
+++ mvc/widgets/LangWidget.php
@@ -2,17 +2,36 @@
 
 namespace Widgets;
 
+use I18n\I18n;
+
 /**
  *
  * @author José María Valera Reales
  *
  */
 class LangWidget extends WidgetBase {
-	/*
-	 * All variables what we need are in Params.globalVars, and
-	 * these are autoimplement in every template.
+
+	/**
+	 * (non-PHPdoc)
 	 *
-	 * So this widget is done :-)
-	 *
+	 * @see \Widgets\WidgetBase::widget()
 	 */
+	public function widget($args, $instance) {
+
+		/*
+		 * Put all languages available to show into the instance var.
+		 */
+		$instance['languages'] = [ ];
+		foreach ( I18n::getAllLangAvailable() as $l ) {
+			$instance['languages'][] = [
+				'key' => $l,
+				'value' => I18n::getLangFullnameBrowser($l)
+			];
+		}
+
+		/*
+		 * And call the widget func from the parent class WidgetBase.
+		 */
+		parent::widget($args, $instance);
+	}
 }
