8fdb461fd7e9ade9020419ebfc524288c39bd09d
Chemaclass <chemaclass@outlook.es>
2015-09-08 14:26:40 +0200

    Add LoginWidget

---
+++ mvc/widgets/LoginWidget.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Widgets;
+
+use I18n\I18n;
+
+/**
+ *
+ * @author José María Valera Reales
+ *
+ */
+class LoginWidget extends WidgetBase {
+	/*
+	 * All variables what we need are in Params.globalVars, and
+	 * these are autoimplement in every template.
+	 *
+	 * So this widget is done :-)
+	 */
+}
