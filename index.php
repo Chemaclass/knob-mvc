1186b1e7472970f75b0c71294a6062491b81ea6c
Chemaclass <chemaclass@outlook.es>
2015-09-30 21:17:43 +0200

    Get the pages from app/pages dir

--- index.php
+++ index.php
@@ -6,17 +6,29 @@
  * If we're in this file means the original file doesn't exists
  * on the root directory. So come on to looking for on Knob default files.
  */
-$query = $GLOBALS['wp_the_query'];
-foreach ( $query as $k => $v ) {
-	if ((substr($k, 0, 3) == 'is_') && $v) {
-		$fileName = substr($k, 3) . '.php';
-		break;
+define('PAGES_DIR', APP_DIR . '/pages/');
+define('VENDOR_KNOB_BASE_DIR', VENDOR_DIR . '/chemaclass/knob-base/');
+
+$wp_the_query = $GLOBALS['wp_the_query'];
+$query = $wp_the_query->query;
+
+if (isset($query) && isset($query['pagename'])) {
+	$fileName = PAGES_DIR . $query['pagename'] . '.php';
+} else {
+	foreach ( $wp_the_query as $k => $v ) {
+		if ((substr($k, 0, 3) == 'is_') && $v) {
+			$fileName = substr($k, 3) . '.php';
+			break;
+		}
 	}
 }
 
-$fileNameInBase = VENDOR_DIR . '/chemaclass/knob-base/' . $fileName;
+$fileNameInBase = VENDOR_KNOB_BASE_DIR . $fileName;
 
-if (file_exists($fileNameInBase)) {
+if (file_exists($fileName)) {
+	// get the file from the knob-base
+	require_once $fileName;
+} else if (file_exists($fileNameInBase)) {
 	// get the file from the knob-base
 	require_once $fileNameInBase;
 } else {
