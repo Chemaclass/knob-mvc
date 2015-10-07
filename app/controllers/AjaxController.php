849283789bc845c4d999c5da66c3740d775b5b80
Chemaclass <chemaclass@outlook.es>
2015-10-07 22:53:11 +0200

    Remove KnobAjaxController

--- app/controllers/AjaxController.php
+++ app/controllers/AjaxController.php
@@ -2,7 +2,6 @@
 
 namespace Controllers;
 
-use Knob\Controllers\AjaxController as KnobAjaxController;
 use Knob\Libs\Ajax;
 use Knob\I18n\I18n;
 use Knob\Libs\KeysRequest;
@@ -10,12 +9,40 @@ use Knob\Models\Post;
 use Knob\Models\Term;
 use Knob\Models\Archive;
 
+// Load WP.
+// We have to require this file, in other case we cant call to the WP functions
+require_once (APP_DIR . '/../../../../wp-load.php');
+
 /**
  * AJAX Controller
  *
  * @author José María Valera Reales
  */
-class AjaxController extends KnobAjaxController {
+class AjaxController extends BaseController {
+
+	/**
+	 * -------------------------------------
+	 * Main Controller for AJAX request
+	 * -------------------------------------
+	 */
+	public function main() {
+		$json = [
+			'code' => 504
+		]; // Error default
+
+		$submit = $_REQUEST['submit'];
+
+		// check if we don't have any submit
+		if (!$submit) {
+			die('');
+		}
+
+		$json = $this->getJsonBySubmit($submit, $_REQUEST);
+
+		// cast the content to UTF-8
+		$json['content'] = mb_convert_encoding($json['content'], "UTF-8");
+		echo json_encode($json);
+	}
 
 	/**
 	 * Listen the home petition
