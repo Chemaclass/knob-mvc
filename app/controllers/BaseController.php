35972a9b5ea04d265064ee75e944933a709af161
Chemaclass <chemaclass@outlook.es>
2015-10-07 23:01:04 +0200

    Remove KnobBaseController. Implement the render method into BaseController

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,7 +2,6 @@
 
 namespace Controllers;
 
-use Knob\Controllers\BaseController as KnobBaseController;
 use Knob\Models\Archive;
 use Knob\Models\Post;
 use Knob\Models\Term;
@@ -15,11 +14,14 @@ use Knob\Libs\Utils;
  *
  * @author José María Valera Reales
  */
-abstract class BaseController extends KnobBaseController {
+abstract class BaseController {
 
 	/*
 	 * Members
 	 */
+	protected $configParams = [ ];
+	protected $currentUser = null;
+	protected $template = null;
 	protected $widgets = [ ];
 	protected $menus = [ ];
 
@@ -27,20 +29,32 @@ abstract class BaseController extends KnobBaseController {
 	 * Constructor
 	 */
 	public function __construct() {
-		parent::__construct();
-
-		/*
-		 * Widgets.
+		/**
+		 *
+		 * @var array
 		 */
+		$this->configParams = Utils::getMustacheParams();
+
+		/**
+		 *
+		 * @var User
+		 */
+		$this->currentUser = User::getCurrent();
+
+		/**
+		 *
+		 * @var Template
+		 */
+		$this->template = Template::getInstance();
+
+		// Widgets
 		foreach ( Template::getDinamicSidebarActive() as $s ) {
 			ob_start();
 			dynamic_sidebar($s);
 			$this->widgets[$s] = ob_get_clean();
 		}
 
-		/*
-		 * Menus.
-		 */
+		// Menus
 		foreach ( Template::getMenusActive() as $s ) {
 			$this->menus[$s] = wp_nav_menu(
 					[
@@ -60,9 +74,7 @@ abstract class BaseController extends KnobBaseController {
 	public function getGlobalVariables() {
 		$globalVars = [ ];
 
-		/*
-		 * Sidebar items
-		 */
+		// Sidebar items
 		$active = ($u = User::getCurrent()) ? $u->isWithSidebar() : User::WITH_SIDEBAR_DEFAULT;
 		$globalVars['widgets'] = [
 			'right' => [
@@ -75,9 +87,7 @@ abstract class BaseController extends KnobBaseController {
 			]
 		];
 
-		/*
-		 * Menus
-		 */
+		// Menus
 		$globalVars['menu'] = [
 			'header' => [
 				'active' => has_nav_menu(Template::MENU_HEADER),
@@ -89,6 +99,38 @@ abstract class BaseController extends KnobBaseController {
 			]
 		];
 
-		return array_merge(parent::getGlobalVariables(), $globalVars);
+		return ($this->configParams + $globalVars);
+	}
+
+	/**
+	 * Render a partial
+	 *
+	 * @param string $templateName
+	 * @param array $templateVars
+	 */
+	public function render($templateName, $templateVars = [], $addGlobalVariables = true) {
+		if ($addGlobalVariables) {
+			$templateVars = array_merge($templateVars, $this->getGlobalVariables());
+		}
+		return $this->template->getRenderEngine()->render($templateName, $templateVars);
+	}
+
+	/**
+	 * Print head + template + footer
+	 *
+	 * @param string $templateName
+	 *        	Template name to print
+	 * @param array $templateVars
+	 *        	Parameters to template
+	 */
+	public function renderPage($templateName, $templateVars = []) {
+		$templateVars = array_merge($templateVars, $this->getGlobalVariables());
+		$addGlobalVariablesToVars = false; // cause we already did it.
+		echo $this->render('head', $templateVars, $addGlobalVariablesToVars);
+		wp_head();
+		echo '</head>';
+		echo $this->render($templateName, $templateVars, $addGlobalVariablesToVars);
+		wp_footer();
+		echo $this->render('footer', $templateVars, $addGlobalVariablesToVars);
 	}
 }
