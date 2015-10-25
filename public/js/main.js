d8bf3076944e4b2f697e98e45b3e1282f3d21037
Chemaclass <chemaclass@outlook.es>
2015-10-25 20:25:37 +0100

    Add licence to main.js file

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 /*
- * Author: Jose Maria Valera Reales
+ * This file is part of the Knob-mvc package.
+ *
+ * (c) José María Valera Reales <chemaclass@outlook.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 var MINIMUM_HEIGHT_FOR_TO_LOAD = 200;
