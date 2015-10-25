5be6f7663433330aaddb959436b16daefb14c01c
Chemaclass <chemaclass@outlook.es>
2015-10-25 21:46:58 +0100

    Create libs/Widgets (extends from Knob-base)

---
+++ app/libs/Widgets.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the Knob-mvc package.
+ *
+ * (c) José María Valera Reales <chemaclass@outlook.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Libs;
+
+use Knob\Libs\Widgets as KnobWidgets;
+
+/**
+ * Widgets to Wordpress
+ *
+ * @author José María Valera Reales
+ */
+class Widgets extends KnobWidgets
+{
+
+    /**
+     * Setup the widgets
+     *
+     * @see KnobWidgets::setup()
+     */
+    public static function setup()
+    {
+        parent::setup();
+        // your widgets
+    }
+}
