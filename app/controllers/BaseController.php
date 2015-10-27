7c82f1052ed5d58bf74c7f20c65ac7da3ebb76e0
Chemaclass <chemaclass@outlook.es>
2015-10-27 23:07:48 +0100

    Create BaseController extends KnobBaseController

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the Knob-mvc package.
+ *
+ * (c) José María Valera Reales <chemaclass@outlook.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Controllers;
+
+use Knob\Controllers\BaseController as KnobBaseController;
+
+/**
+ * Base Controller
+ *
+ * @author José María Valera Reales
+ */
+class BaseController extends KnobBaseController
+{
+}
