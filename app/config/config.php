aea3961221e7f15f7acaf034886e79de17515c34
Chemaclass <chemaclass@outlook.es>
2015-10-28 22:59:45 +0100

    Change the name from config files. snake_case instead of CamelCase because are config file, not classes

---
+++ app/config/config.php
@@ -0,0 +1,46 @@
+<?php
+/*
+* This file is part of the Knob-mvc package.
+*
+* (c) José María Valera Reales <chemaclass@outlook.es>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+/**
+* ============================
+* Your global config file
+* ============================
+*/
+return [
+    /**
+    * ====================
+    * Environments
+    * ====================
+    */
+    'env' => [
+        'prod' => 'knob.chemaclass.com',
+        'dev' => 'knob.chemaclass.dev',
+        'loc' => 'knob.chemaclass.local'
+    ],
+
+    /**
+    * ====================
+    * Languages available
+    * ====================
+    */
+    'langAvailable' => [
+        'en' => 'english',
+        'es' => 'español',
+        'de' => 'deutsch'
+    ],
+
+    /**
+    * ====================
+    * Language by default
+    * ====================
+    */
+    'langDefault' => 'en',
+
+];
---
+++ app/config/mustache_helpers.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* This file is part of the Knob-mvc package.
+*
+* (c) José María Valera Reales <chemaclass@outlook.es>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace Config;
+
+use Knob\I18n\I18n;
+
+/**
+* ============================
+* Your Mustache helpers
+* ============================
+*
+* @see knob-base/src/config/mustache_helpers.php -> Parent file
+*
+*      ----------------------------
+*      For example:
+*      ----------------------------
+*      $lower_text = 'lower text to upper'; // var from PHP code
+*
+*      {{#case.upper}} lower_text {{/case.upper}} -> LOWER TEXT TO UPPER
+*      Or
+*      {{ lower_text | case.upper}} -> LOWER TEXT TO UPPER
+*
+*
+* @link https://github.com/bobthecow/mustache.php#usage
+* @link https://github.com/bobthecow/mustache.php/wiki/FILTERS-pragma
+*/
+return [
+    'case' => [
+        'lower' => function ($value)
+        {
+            return strtolower((string) $value);
+        },
+        'upper' => function ($value)
+        {
+            return strtoupper((string) $value);
+        }
+    ]
+];
