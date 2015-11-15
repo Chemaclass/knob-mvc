179d2bae36b11f384544e73ab869d3f6c7a23470
Chemaclass <chemaclass@outlook.es>
2015-11-15 18:09:25 +0100

    Add param ajaxUrl as optional value

--- app/config/mustache_params.php
+++ app/config/mustache_params.php
@@ -1,22 +1,25 @@
 <?php
 /*
-* This file is part of the Knob-mvc package.
-*
-* (c) José María Valera Reales <chemaclass@outlook.es>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * This file is part of the Knob-mvc package.
+ *
+ * (c) José María Valera Reales <chemaclass@outlook.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 use Knob\I18n\I18n;
 
 /**
-* ============================
-* Your Mustache params
-* ============================
-*
-* @see knob-base/src/config/mustache_params.php -> Parent file
-*/
+ * ============================
+ * Your Mustache params
+ * ============================
+ *
+ * @see knob-base/src/config/mustache_params.php -> Parent file
+ *
+ */
 return [
+    'ajaxUrl' => '/ajax',
+
     'blogAuthor' => 'José María Valera Reales',
     'blogDescription' => ($d = I18n::trans('internal.blog_description')) ? $d : get_bloginfo('description'),
     'blogKeywords' => 'knob, wordpress, framework, mvc, template, mustache, php'
